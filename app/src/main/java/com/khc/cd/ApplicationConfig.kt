19ae5575f729fb3f6c12c95b5d64a39523c96317
Seungmin-develop <tmdals8798@gmail.com>
2023-07-05 16:52:35 +0900

    cd 테스트 진행중

---
+++ app/src/main/java/com/khc/cd/ApplicationConfig.kt
@@ -0,0 +1,11 @@
+package com.khc.cd
+
+class ApplicationConfig {
+
+    companion object {
+        private const val APPLICATION_NAME = "KHC-Application/1.0"
+        private const val PACKAGE_NAME = "com.khc.cicdtestapp"
+        private const val APK_FILE_PATH = "/"
+    }
+
+}
